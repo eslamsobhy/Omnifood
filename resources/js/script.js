d5e99d8b018f780cff1b487ea9d3de5c124a98e2
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-14 19:02:31 +0200

    reviewed the steps!

--- resources/js/script.js
+++ resources/js/script.js
@@ -11,3 +11,11 @@ $(document).ready(function () {
     { offset: "140px" }
   );
 });
+
+// The problem:
+// - we want to toggle the class once we enter a specific section
+// - we used waypoints js plugin to do so
+// - once we enter the desired section (".js--features"), we will toggle the class
+// - the direction parameter determines either we are scrolling up, or down
+// - the object passed with the offset property makes us decide where exactly to do the toggle
+// - {offset: 140px} => will toggle before entering the desired section by 140px
