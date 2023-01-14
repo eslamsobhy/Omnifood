766e58cd71db32709bf80f4eb5f4310f17838fb6
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-14 18:56:41 +0200

    completed implementing the sticky nav feature

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,13 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  // class toggling feature using waypoint to determine where to toggle!
+  $(".js--features").waypoint(
+    function (direction) {
+      if (direction === "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    { offset: "140px" }
+  );
+});
